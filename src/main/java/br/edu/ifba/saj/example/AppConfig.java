57267f1ae345bf100428a2297482d12cb2520a55
quarkusio <no-reply@quarkus.io>
2024-10-16 11:32:42 +0000

    Initial commit

---
+++ src/main/java/br/edu/ifba/saj/example/AppConfig.java
@@ -0,0 +1,8 @@
+package br.edu.ifba.saj.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("starter-theme")
+public class AppConfig implements AppShellConfigurator {
+}
